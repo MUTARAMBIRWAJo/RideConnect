83ede0a57d94f68741ea0b7503d92b6da5cf19d8
Engineer Joseph <139768948+MUTARAMBIRWAJo@users.noreply.github.com>
2026-03-25 17:42:38 +0200

    Fix OfficerDashboard Livewire root element error by adding single-root Blade file

---
+++ resources/views/filament/pages/officer-dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-filament-panels::page>
+    {{-- Officer dashboard widgets/components go here --}}
+    {{-- Example: --}}
+    {{-- <x-filament-widgets::widgets :widgets="$this->getHeaderWidgets()" /> --}}
+</x-filament-panels::page>
