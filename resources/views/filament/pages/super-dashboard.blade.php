10ef0fc67df6f013ee4642111ed6988b391d7210
Engineer Joseph <139768948+MUTARAMBIRWAJo@users.noreply.github.com>
2026-04-04 10:01:51 +0200

    fix: enforce strict single-root super dashboard blade

--- resources/views/filament/pages/super-dashboard.blade.php
+++ resources/views/filament/pages/super-dashboard.blade.php
@@ -1,20 +1,11 @@
 <x-filament-panels::page class="fi-dashboard-page">
     <div class="space-y-6">
-        @if (method_exists($this, 'filtersForm'))
-            <div>
-                {{ $this->filtersForm }}
-            </div>
-        @endif
-
-        <x-filament-widgets::widgets
-            :columns="$this->getColumns()"
-            :data="
-                [
-                    ...(property_exists($this, 'filters') ? ['filters' => $this->filters] : []),
-                    ...$this->getWidgetData(),
-                ]
-            "
-            :widgets="$this->getVisibleWidgets()"
-        />
+        <div>
+            <x-filament-widgets::widgets
+                :columns="$this->getColumns()"
+                :data="$this->getWidgetData()"
+                :widgets="$this->getVisibleWidgets()"
+            />
+        </div>
     </div>
 </x-filament-panels::page>
