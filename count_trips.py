39a053778e58c26f1321155b121814e0daeedcc4
Joseph <joseph@example.com>
2026-06-19 12:08:40 +0200

    security: update database connection credentials

--- count_trips.py
+++ count_trips.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine, text
-engine = create_engine("postgresql+psycopg2://postgres.tpahuvmhlfluztuhznfj:rOnptMsAAnTbrpIY@aws-1-us-east-1.pooler.supabase.com:5432/postgres?sslmode=require")
+engine = create_engine("postgresql+psycopg2://postgres.tpahuvmhlfluztuhznfj:k66JhsiIhCBkPcs3@aws-1-us-east-1.pooler.supabase.com:5432/postgres?sslmode=require")
 with engine.connect() as conn:
     print(list(conn.execute(text("SELECT count(*) FROM trips"))))
